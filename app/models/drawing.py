f140b3a5e310967c07d67449c8cb1944ba41f47e
trieuH <115166533+trieuH@users.noreply.github.com>
2024-04-27 21:01:35 +0800

    Create models folder and add model files for drawing, guess, and word

---
+++ app/models/drawing.py
@@ -0,0 +1,15 @@
+from app import db
+
+class Drawing(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word_id = db.Column(db.Integer, db.ForeignKey('word.id'))
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    drawing_data = db.Column(db.LargeBinary) # TODO: decide whether to store drawings as BLOB or path to file stored on server
+    created_at = db.Column(db.DateTime)
+
+    word = db.relationship('Word', back_populates='drawing')
+    creator = db.relationship('User', back_populates='drawing')
+    guess = db.relationship('Guess', back_populates='drawing')
+
+    def __repr__(self):
+        return(f'<Drawing "id={self.id}, word_id={self.word_id}, creator_id={self.creator_id}">')
---
+++ app/models/guess.py
@@ -0,0 +1,18 @@
+from app import db
+
+class Guess (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    drawing_id = db.Column(db.Integer, db.ForeignKey('drawing.id'))
+    guesser_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    is_correct = db.Column(db.Boolean)
+    points_for_creator = db.Column(db.Integer, default=0)
+    points_for_guesser = db.Column(db.Integer, default=0)
+    time_shown = db.Column(db.DateTime)
+    time_guessed = db.Column(db.DateTime)
+    time_taken = db.Column(db.Float)
+
+    drawing = db.relationship('Drawing', back_populates='guess')
+    guesser = db.relationship('User', back_populates='guesser')
+
+    def __repr__(self):
+        return(f'<Guess "id={self.id}, drawing_id={self.drawing_id}, guesser_id={self.guesser_id}">')
