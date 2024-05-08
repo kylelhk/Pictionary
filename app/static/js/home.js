801e8491f5cac231137d2e7a47372e2c1fd320d0
Kyle Leung <lkkc0300@gmail.com>
2024-05-08 22:19:29 +0800

    Add a pie chart for point summary, adjust overall format

--- app/static/js/home.js
+++ app/static/js/home.js
@@ -23,4 +23,53 @@ $(function () {
 
   // Initialise the tabs
   initialiseTabs();
+
+  /* Pie chart implemented using ECharts library */
+  var pieContainer = document.querySelector(".pie-container");
+  if (pieContainer) {
+    var pieChart = echarts.init(pieContainer);
+    var pieOption = {
+      title: {
+        text: "Points Distribution",
+        left: "center",
+      },
+      tooltip: {
+        trigger: "item",
+        formatter: "{a} <br/>{b} : {c} ({d}%)",
+      },
+      legend: {
+        orient: "vertical",
+        left: "right",
+        top: "center",
+        data: ["Guesser Points", "Drawer Points"],
+      },
+      series: [
+        {
+          name: "Points",
+          type: "pie",
+          radius: "75%", // Increase the radius to make the pie bigger
+          center: ["35%", "54%"], // 30% from left, 54% from top
+          data: [
+            { value: 53, name: "Guesser Points", itemStyle: { color: "Blue" } },
+            {
+              value: 47,
+              name: "Drawer Points",
+              itemStyle: { color: "Orange" },
+            },
+          ],
+          emphasis: {
+            itemStyle: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: "rgba(0, 0, 0, 0.5)",
+            },
+          },
+          label: {
+            show: false,
+          },
+        },
+      ],
+    };
+    pieChart.setOption(pieOption);
+  }
 });
