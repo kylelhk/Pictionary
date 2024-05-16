8913fe331d46d01a04cd137aced54cbf37e02203
trieuH <115166533+trieuH@users.noreply.github.com>
2024-05-16 20:13:19 +0800

    Add basic test to ensure functionality

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,20 +6,33 @@ from app.models import User
 
 # Add test cases here
 
+class LoginTests(TestCase):
+    def create_app(self):
+        return create_app("TestConfig")
 
-class BasicTests(TestCase):
     def setUp(self):
-        test_app = create_app(TestConfig)
-        self.app_context = test_app.app_context()
+        self.app = self.create_app()
+        self.app_context = self.app.app_context()
         self.app_context.push()
-        # db.create_all()
-        # add_test_data_to_db()
+        db.create_all()
+        self.add_test_data_to_db()
 
     def tearDown(self):
-        # db.session.remove()
-        # db.drop_all()
+        db.session.remove()
+        db.drop_all()
         self.app_context.pop()
 
+    def add_test_data_to_db(self):
+        user1 = User(username="user1", email="user1@example.com")
+        user1.set_password("password")
+        db.session.add(user1)
+        db.session.commit()
+
+    def test_password_hashing(self):
+        u = User.query.filter_by(username="user1").first()
+        self.assertIsNotNone(u)
+        self.assertTrue(u.check_password("password"))
+        self.assertFalse(u.check_password("incorrect"))
 
 if __name__ == "__main__":
     unittest.main()
