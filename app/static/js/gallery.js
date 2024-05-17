5750d9771a9feb35a9a0c763b346b80781fc8cdd
Kyle Leung <lkkc0300@gmail.com>
2024-05-17 21:39:59 +0800

    Improve search function, fix pagination position, increase number of drawings shown per page, remove cdn in css

--- app/static/js/gallery.js
+++ app/static/js/gallery.js
@@ -1,45 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
-    fetchAndDisplayGallery();
+document.addEventListener("DOMContentLoaded", function () {
+  fetchAndDisplayGallery();
 });
 
-let currentData = [];  // Store fetched data
+let currentData = []; // Store fetched data
 let displayedData = []; // Data currently displayed, could be filtered
 let currentPage = 1;
-const perPage = 9;  // Items per page
+const perPage = 10; // Drawings per page
 
 function fetchAndDisplayGallery() {
-    const apiUrl = `/get-gallery-data`;
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Sort data by date_created in descending order (newest first)
-            data.sort((a, b) => new Date(b.date_created) - new Date(a.date_created));
-            currentData = data;  // Store data
-            displayedData = data; // Initially displayed data is all data
-            displayPage(currentPage);  // Display first page
-        })
-        .catch(error => console.error('Error loading the gallery data:', error));
+  const apiUrl = `/get-gallery-data`;
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      // Sort data by date_created in descending order (newest first)
+      data.sort((a, b) => new Date(b.date_created) - new Date(a.date_created));
+      currentData = data; // Store data
+      displayedData = data; // Initially displayed data is all data
+      displayPage(currentPage); // Display first page
+    })
+    .catch((error) => console.error("Error loading the gallery data:", error));
 }
 
 function displayPage(page) {
-    const startIndex = (page - 1) * perPage;
-    const endIndex = startIndex + perPage;
-    const pageData = displayedData.slice(startIndex, endIndex);
-    updateGalleryTable(pageData);
-    updatePagination(page, Math.ceil(displayedData.length / perPage));
+  const startIndex = (page - 1) * perPage;
+  const endIndex = startIndex + perPage;
+  const pageData = displayedData.slice(startIndex, endIndex);
+  updateGalleryTable(pageData);
+  updatePagination(page, Math.ceil(displayedData.length / perPage));
 }
 
 function updateGalleryTable(data) {
-    const tableBody = document.getElementById('galleryTableBody');
-    tableBody.innerHTML = '';
-    data.forEach(item => {
-        const row = createTableRow(item);
-        tableBody.innerHTML += row;
-    });
+  const tableBody = document.getElementById("galleryTableBody");
+  tableBody.innerHTML = "";
+  data.forEach((item) => {
+    const row = createTableRow(item);
+    tableBody.innerHTML += row;
+  });
 }
 
 function createTableRow(item) {
-    return `<tr>
+  return `<tr>
                 <td><button class="btn btn-primary btn-sm" onclick="openDrawing(${item.drawing_id})">Open</button></td>
                 <td>${item.creator}</td>
                 <td>${item.category}</td>
@@ -49,51 +49,84 @@ function createTableRow(item) {
 }
 
 function openDrawing(drawingId) {
-    // Assuming the drawing detail page URL pattern is "/drawings/{id}"
-    window.location.href = `/drawings/${drawingId}`;
+  // Assuming the drawing detail page URL pattern is "/drawings/{id}"
+  window.location.href = `/drawings/${drawingId}`;
 }
 
 function updatePagination(currentPage, totalPages) {
-    const paginationContainer = document.querySelector('.pagination');
-    paginationContainer.innerHTML = '';
+  const paginationContainer = document.querySelector(".pagination");
+  paginationContainer.innerHTML = "";
 
-    paginationContainer.innerHTML += `<li class="page-item ${currentPage === 1 ? 'disabled' : ''}">
-        <a class="page-link" href="#" onclick="${currentPage > 1 ? 'navigatePage(' + (currentPage - 1) + ')' : 'event.preventDefault()'}">Previous</a>
+  paginationContainer.innerHTML += `<li class="page-item ${
+    currentPage === 1 ? "disabled" : ""
+  }">
+        <a class="page-link" href="#" onclick="${
+          currentPage > 1
+            ? "navigatePage(" + (currentPage - 1) + ")"
+            : "event.preventDefault()"
+        }">Previous</a>
     </li>`;
 
-    for (let i = 1; i <= totalPages; i++) {
-        paginationContainer.innerHTML += `<li class="page-item ${i === currentPage ? 'active' : ''}">
+  for (let i = 1; i <= totalPages; i++) {
+    paginationContainer.innerHTML += `<li class="page-item ${
+      i === currentPage ? "active" : ""
+    }">
             <a class="page-link" href="#" onclick="navigatePage(${i})">${i}</a>
         </li>`;
-    }
+  }
 
-    paginationContainer.innerHTML += `<li class="page-item ${currentPage === totalPages ? 'disabled' : ''}">
-        <a class="page-link" href="#" onclick="${currentPage < totalPages ? 'navigatePage(' + (currentPage + 1) + ')' : 'event.preventDefault()'}">Next</a>
+  paginationContainer.innerHTML += `<li class="page-item ${
+    currentPage === totalPages ? "disabled" : ""
+  }">
+        <a class="page-link" href="#" onclick="${
+          currentPage < totalPages
+            ? "navigatePage(" + (currentPage + 1) + ")"
+            : "event.preventDefault()"
+        }">Next</a>
     </li>`;
 }
 
 function navigatePage(page) {
-    currentPage = page;
-    displayPage(page);
-    window.scrollTo(0, 0); // Scroll to the top of the page on page change
+  currentPage = page;
+  displayPage(page);
+  window.scrollTo(0, 0); // Scroll to the top of the page on page change
 }
 
 // Function to filter data based on search query
 function filterGalleryData() {
-    const searchTerm = document.querySelector('.search-bar input').value.toLowerCase();
-    if (!searchTerm) {
-        displayedData = currentData; // Reset to all data if search term is empty
-    } else {
-        const terms = searchTerm.split(' '); // Split search term into individual words
-        displayedData = currentData.filter(item => terms.some(term => item.creator.toLowerCase().includes(term) || item.category.toLowerCase().includes(term) || item.status.toLowerCase().includes(term) || item.date_created.includes(term) // Include date in the search
-        ));
-    }
-    currentPage = 1; // Reset to first page
-    displayPage(currentPage);
+  const searchTerm = document
+    .querySelector(".search-bar input")
+    .value.toLowerCase();
+  if (!searchTerm) {
+    displayedData = currentData; // Reset to all data if search term is empty
+  } else {
+    const terms = searchTerm.split(" "); // Split search term into individual words
+    displayedData = currentData.filter((item) =>
+      terms.some(
+        (term) =>
+          item.creator.toLowerCase().includes(term) ||
+          item.category.toLowerCase().includes(term) ||
+          item.status.toLowerCase().includes(term) ||
+          item.date_created.includes(term) // Include date in the search
+      )
+    );
+  }
+  currentPage = 1; // Reset to first page
+  displayPage(currentPage);
 }
 
 // Adding an event listener to the search button
-document.getElementById('button-addon2').addEventListener('click', function () {
-    filterGalleryData();
-    window.scrollTo(0, 0); // Scroll to the top of the page after filtering
+document.getElementById("button-addon2").addEventListener("click", function () {
+  filterGalleryData();
+  window.scrollTo(0, 0); // Scroll to the top of the page after filtering
 });
+
+// Adding an event listener for the Enter key to trigger the search button click
+document
+  .getElementById("search-input")
+  .addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault(); // Prevent the default form submission
+      document.getElementById("button-addon2").click(); // Trigger the button click
+    }
+  });
